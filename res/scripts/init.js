4a6d08525c58b49262f132309b64ebb8d1691f90
Shivansh Rakesh <iamshivansh.2000@gmail.com>
2020-07-25 23:41:05 +0530

    made launch animation and navbar responsive

--- res/scripts/init.js
+++ res/scripts/init.js
@@ -6,7 +6,8 @@
 
 function init() {
     $("#camera-gif-figure").animate({
-        top: "30%",
+        // width and max-width have to be changed accordingly in index.css > #camera-gif
+        top: String(0.5 * $(window).height() - (Math.min(0.15 * $(window).width(), 150))) + "px",
         opacity: "1",
     });
 
