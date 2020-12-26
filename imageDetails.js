39447bdb76583c148a94251a0a8475335fdee192
Rishabh Khanna <55993921+KyaBacchaHai@users.noreply.github.com>
2020-12-26 11:14:02 +0530

    Added script for updating creative eye carousel
    Added Js carousel to Previous Contest Winners (#19)

---
+++ imageDetails.js
@@ -0,0 +1,38 @@
+var imageDetails = [
+    {
+        "url":"./res/images/c-eye-past-winners/August_Brijesh.jpg",
+        "caption":"There is no light at the end of the tunnel. There is only you. You are the light.",
+        "author":"Brijesh Singh",
+        "month":"August\'20"
+    },
+    {
+        "url":"./res/images/c-eye-past-winners/Jalpreet_July.jpg",
+        "caption":"Even the Darkest Night will End and the Sun will Rise.. ",
+        "author":"Jalpreet Singh Nanda",
+        "month":"July\'20"
+    },
+    {
+        "url":"./res/images/c-eye-past-winners/June_Batu_Caves.png",
+        "caption":"Batu Caves, Malaysia",
+        "author":"Meher Shashwat Nigam",
+        "month":"June\'20"
+    },
+    {
+        "url":"./res/images/c-eye-past-winners/May_Visitor.png",
+        "caption":"Visitor",
+        "author":"Indranil Pradhan",
+        "month":"May\'20"
+    },
+    {
+        "url":"./res/images/c-eye-past-winners/April_Ranges_and_Ganges.png",
+        "caption":"Ranges and Ganges",
+        "author":"Shivam Bansal",
+        "month":"April\'20"
+    },
+    {
+        "url":"./res/images/c-eye-past-winners/March_Felicity_2020.png",
+        "caption":"DJ Night @Felicity2020",
+        "author":"Sai Vineesh C",
+        "month":"March\'20"
+    }
+]
